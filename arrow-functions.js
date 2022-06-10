633b4bcc8b224009fb8411b2037835192529b549
marktodman <drmtodman@gmail.com>
2022-06-10 15:22:35 +0000

    Final challenge

--- arrow-functions.js
+++ arrow-functions.js
@@ -5,10 +5,43 @@
 
 // Vanilla JavaScript Function
 
+// function addTwoNumbers(a, b) {
+//     return a + b;
+// }
+
+// let sum = addTwoNumbers(3, 5)
+// console.log(sum)
+
 // Arrow Function With Parameters
+
+const addTwoNumbers = (a, b) => {
+    return a + b;
+}
+
+let sum = addTwoNumbers(4, 5)
+console.log(sum)
 
 // Single Line Arrow Function With Parameters
 
+const addTwoNumbers2 = (a, b) => a + b;
+
+let num = addTwoNumbers(5, 5)
+console.log(num)
+
 // Implicit Returns
 
+const saySomething = message => console.log(message);
+saySomething('Hello, World!')
+
+const sayHello = () => console.log('Hello');
+sayHello();
+
 // Returning Multiple Lines
+
+const returnMultipleLines = () => (
+    `<p>
+    This is a multiline string!
+    <p>`
+)
+
+console.log(returnMultipleLines());
